199b95929d88ea9e052eeaba61c28c9421690beb
Fillipe <fillipe484@gmail.com>
2021-09-02 21:39:24 -0300

    feat: Header - using responsive fontsize and icons

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import {
+  Container,
+  UserWrapper,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
 
-import { Container, Title } from './styles'
+} from './styles'
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>
-        Dashboard
-      </Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/38387593?v=4' }} />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Fillipe</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
