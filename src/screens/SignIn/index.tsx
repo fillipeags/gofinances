1138429c8c2be200839b92ab1cc107d0d2e5065e
Fillipe <fillipe484@gmail.com>
2021-09-07 14:35:07 -0300

    feat: connecting with a google account

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Alert } from 'react-native'
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useAuth } from '../../hooks/auth';
 
@@ -19,8 +20,16 @@ import {
 } from './styles'
 
 export function SignIn() {
-  const { user } = useAuth();
-  console.log(user);
+  const { signInWithGoogle } = useAuth();
+
+  async function handleSignInWithGoogle() {
+    try {
+      await signInWithGoogle();
+    } catch (err) {
+      console.log(err)
+      Alert.alert('Não foi possível conectar a conta Google')
+    }
+  }
 
   return (
     <Container>
@@ -43,6 +52,7 @@ export function SignIn() {
           <SignInSocialButton
             title="Entrar com Google"
             svg={GoogleSvg}
+            onPress={handleSignInWithGoogle}
           />
           <SignInSocialButton
             title="Entrar com Apple"
