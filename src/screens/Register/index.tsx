69cd62b3f8d702c42377b20b20e25da08c87fd3e
Fillipe <fillipe484@gmail.com>
2021-10-06 12:40:38 -0300

    feat: fire event on test

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -170,6 +170,7 @@ export function Register() {
               />
             </TransactionsTypes>
             <CategorySelectButton
+              testID="button-category"
               title={category.name}
               onPress={handleOpenSelectCategoryModal}
             />
@@ -180,7 +181,7 @@ export function Register() {
           />
         </Form>
 
-        <Modal visible={categoryModalOpen}>
+        <Modal testID="modal-category" visible={categoryModalOpen}>
           <CategorySelect
             category={category}
             setCategory={setCategory}
